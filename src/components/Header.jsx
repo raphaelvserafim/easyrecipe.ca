680d4c8fc42a4bed322d4a067b45ab9a5fa76e5a
Raphael Serafim <raphaelvserafim@gmail.com>
2023-03-25 12:44:19 -0400

    feat: Button Is Logged

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,26 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-
-
 import Menu from './Menu';
+import ButtonIsLogged from './ButtonIsLogged';
+
+const nameApp = 'Easy Recipe'
+const log = 1;
 
 export default function Header() {
+    let ButtonLogin = <Button color="inherit">Login</Button>;
+    if (log) {
+        ButtonLogin = <ButtonIsLogged />;
+    }
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="fixed">
                 <Toolbar>
                     <Menu />
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
+                        {nameApp}
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    {ButtonLogin}
                 </Toolbar>
             </AppBar>
         </Box>
