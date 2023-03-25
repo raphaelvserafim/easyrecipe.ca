43bacd3206a4873e7da9ffd804826cd4533f4070
Raphael Serafim <raphaelvserafim@gmail.com>
2023-03-25 14:26:45 -0400

    feat: CardSkeleton

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,16 @@ import * as React from 'react';
 import SEO from '../SEO';
 import SearchInput from '../components/SearchInput';
 
+
+import CardSkeleton from '../components/CardSkeleton';
+
 function Home() {
     console.log("Page Home")
     SEO({ title: 'Home' });
     return (<>
-
         <SearchInput />
+        <CardSkeleton loading={true} />
+        <CardSkeleton loading={true} />
     </>
 
     );
