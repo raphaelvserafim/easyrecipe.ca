7d01689f8f4d83993cb27d47689f322f8e287add
Raphael Serafim <raphaelvserafim@gmail.com>
2023-03-25 16:08:49 -0400

    refactor: lines

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,23 +10,24 @@ import CardSkeleton from '../components/CardSkeleton';
 
 function Home() {
     console.log("Page Home")
-    
+
     SEO({ title: 'Home' });
 
-    const dataCard = { title: "Is Test", dateTime: "Today", coverImage: "", avatar: "", author: "Raphael Serafim" }
+    //const dataCard = { title: "Is Test", dateTime: "Today", coverImage: "", avatar: "", author: "Raphael Serafim" }
 
-    return (<>
-        <SearchInput />
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={0} >
-                {[...Array(6)].map((_, index) => (
-                    <Grid key={index} xs={12} sm={6} md={4}>
-                        <CardSkeleton loading={true} />
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
-    </>
+    return (
+        <>
+            <SearchInput />
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={0} >
+                    {[...Array(6)].map((_, index) => (
+                        <Grid key={index} xs={12} sm={6} md={4}>
+                            <CardSkeleton loading={true} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
+        </>
 
     );
 
