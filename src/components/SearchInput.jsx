12de4364413430b221bad0bb453bb86ff8746b88
Raphael Serafim <raphaelvserafim@gmail.com>
2023-03-25 16:30:29 -0400

    feat: ResultSearch

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,24 +1,51 @@
-import * as React from 'react';
+import { useState } from 'react';
+import { useNavigate } from '@reach/router';
 import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 
+function SearchInput(props) {
+    const [searchTerm, setSearchTerm] = useState(props.value || "");
 
-function SearchInput() {
+    const navigate = useNavigate();
+
+    const handleSearch = () => {
+        if (searchTerm) {
+            navigate(`/search/${searchTerm}`);
+        }
+    };
+
+    const handleInputChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
+    };
+
     return (
         <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: "100%" }} >
             <InputBase
                 sx={{ ml: 1, flex: 1 }}
                 placeholder="Search for a recipe"
                 inputProps={{ 'aria-label': 'search for a recipe' }}
+                value={searchTerm}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
             />
-            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+            <IconButton
+                type="button"
+                sx={{ p: '10px' }}
+                aria-label="search"
+                onClick={handleSearch}
+            >
                 <SearchIcon />
             </IconButton>
         </Paper>
     );
-
 }
 
 export default SearchInput;
---
+++ src/pages/ResultSearch.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Grid from '@mui/material/Unstable_Grid2';
+import Box from '@mui/material/Box';
+import { useParams } from '@reach/router';
+
+
+import SEO from '../SEO';
+import SearchInput from '../components/SearchInput';
+
+import CardSkeleton from '../components/CardSkeleton';
+
+function ResultSearch() {
+    const { name } = useParams();
+    SEO({ title: 'Result Search' });
+
+    console.log("name", name)
+
+
+    return (
+        <>
+        
+            <SearchInput value={name} />
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={0} >
+                    {[...Array(6)].map((_, index) => (
+                        <Grid key={index} xs={12} sm={6} md={4}>
+                            <CardSkeleton loading={true} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
+        </>
+
+    );
+
+}
+
+export default ResultSearch;
