cdc8c80d2a69992013c073e6fe693217a926aa5b
Raphael Serafim <raphaelvserafim@gmail.com>
2023-03-25 17:00:27 -0400

    refactor: Container

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -4,6 +4,8 @@ import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
+import Container from '@mui/material/Container';
+
 
 function SearchInput(props) {
     const [searchTerm, setSearchTerm] = useState(props.value || "");
@@ -27,24 +29,27 @@ function SearchInput(props) {
     };
 
     return (
-        <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: "100%" }} >
-            <InputBase
-                sx={{ ml: 1, flex: 1 }}
-                placeholder="Search for a recipe"
-                inputProps={{ 'aria-label': 'search for a recipe' }}
-                value={searchTerm}
-                onChange={handleInputChange}
-                onKeyDown={handleKeyDown}
-            />
-            <IconButton
-                type="button"
-                sx={{ p: '10px' }}
-                aria-label="search"
-                onClick={handleSearch}
-            >
-                <SearchIcon />
-            </IconButton>
-        </Paper>
+        <Container>
+            <Paper component="form" sx={{ p: '2px 2px', display: 'flex', alignItems: 'center', width: "100%" }} >
+                <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="Search for a recipe"
+                    inputProps={{ 'aria-label': 'search for a recipe' }}
+                    value={searchTerm}
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                />
+                <IconButton
+                    type="button"
+                    sx={{ p: '10px' }}
+                    aria-label="search"
+                    onClick={handleSearch}
+                >
+                    <SearchIcon />
+                </IconButton>
+            </Paper>
+
+        </Container>
     );
 }
 
