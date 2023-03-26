28a9e4b25cc3f92644c74036b071b378b879fdf6
Raphael Serafim <raphaelvserafim@gmail.com>
2023-03-26 12:05:03 -0400

    fix: bug navigate [object%20Object]

--- src/components/BackPage.jsx
+++ src/components/BackPage.jsx
@@ -3,11 +3,18 @@ import { useNavigate } from '@reach/router';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import Button from '@mui/material/Button';
 
-
 function BackPage() {
+  const navigate = useNavigate();
 
-  return (<Button onClick={useNavigate(-1)}><ArrowBackIosIcon /></Button>);
+  const handleClick = () => {
+    navigate(-1);
+  };
 
+  return (
+    <Button onClick={handleClick}>
+      <ArrowBackIosIcon />
+    </Button>
+  );
 }
 
 export default BackPage;
